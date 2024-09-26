10572192a6dcdf8fdc61b6dc95b1924119d39ddd
Parmeet <parmeet202005@gmail.com>
2024-09-26 21:29:16 +0530

    bug fixes

--- src/main/java/com/estate/est/controllers/PropertyController.java
+++ src/main/java/com/estate/est/controllers/PropertyController.java
@@ -1,17 +1,27 @@
 package com.estate.est.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.estate.est.dto.ApiResponse;
 import com.estate.est.dto.CreatePropertyDto;
 import com.estate.est.entities.Property;
 import com.estate.est.service.EmailService;
 import com.estate.est.service.implementations.PropertyServiceImpl;
 import com.estate.est.service.implementations.UserServiceImpl;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("api/properties")
